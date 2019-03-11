fdbf79ee598a5078799b78459be8e65d0559dc06
Thomas Herrera <rayherrera90@outlook.com>
2019-03-10 23:50:27 -0600

    Added Junit 5 test.

--- src/edu/cnm/deepdive/Matrices.java
+++ src/edu/cnm/deepdive/Matrices.java
@@ -4,14 +4,14 @@ public class Matrices {
 
   public static int[][] rotate(int[][] data) {
 
-    int[][] rotated = new int[data.length][data.length];
+    int[][] Rotated = new int[data.length][data.length];
 
     for (int i = 0; i < data.length; i++) {
       for (int j = 0; j < data.length; j++){
-        rotated[i][j] = data[data.length - 1 - j][i];
+        Rotated[i][j] = data[data.length - 1 - j][i];
       }
     }
-    return rotated;
+    return Rotated;
   }
 
 }
--- test/edu/cnm/deepdive/MatricesTest.java
+++ test/edu/cnm/deepdive/MatricesTest.java
@@ -6,7 +6,19 @@ import org.junit.jupiter.api.Test;
 
 class MatricesTest {
 
+  private static final int[][] Input1 = {{-1}};
+  private static final int[][] Input2 = {{1,2},{4,3}};
+  private static final int[][] Input3 = {{2,4,6},{8,10,12},{14,16,18}};
+  private static final int[][] Expected1 = {{-1}};
+  private static final int[][] Expected2 = {{4,1},{3,2}};
+  private static final int[][] Expected3 = {{14,8,2},{16,10,4},{18,12,6}};
+
   @Test
   void rotate() {
+
+    assertArrayEquals(Expected1, Matrices.rotate(Input1));
+    assertArrayEquals(Expected2, Matrices.rotate(Input2));
+    assertArrayEquals(Expected3, Matrices.rotate(Input3));
+
   }
 }
